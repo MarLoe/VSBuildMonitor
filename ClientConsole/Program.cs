1dd2aa44d5b62ae870c8c94745ffd77516ca55bd
Martin Løbger <martin@lobger.com>
2023-06-12 22:08:12 +0200

    Adding console client for testing purposes

---
+++ ClientConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using com.lobger.WebSocketServer;
+
+namespace ClientConsole;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var server = new WebSocketServer("http://+:11011/");
+        await server.Start();
+    }
+}
+
