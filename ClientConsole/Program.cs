ea711368a4d116a38051d25578273ba35bb30ce8
Martin Løbger <martin@lobger.com>
2023-06-12 22:08:12 +0200

    Testing request manager from console

--- ClientConsole/Program.cs
+++ ClientConsole/Program.cs
@@ -1,4 +1,7 @@
-﻿using com.lobger.WebSocketServer;
+﻿using System.Text;
+using System.Text.Json;
+using com.lobger.WebSocketServer;
+using com.lobger.WebSocketServer.Messages;
 
 namespace ClientConsole;
 
@@ -6,6 +9,35 @@ class Program
 {
     static async Task Main(string[] args)
     {
+        var request = new Message<HandshakeRequest>
+        {
+            Type = "request",
+            Uri = "handshake",
+            Payload = new() { Token = "TheToken" },
+        };
+
+        var requestManager = new RequestManager();
+        requestManager.RegisterRequest("subscribe", (c, m, t) =>
+        {
+            Console.WriteLine($@"Client {c}");
+            return Task.CompletedTask;
+        });
+        requestManager.RegisterRequest<HandshakeRequest>("handshake", (c, m, t, r) =>
+        {
+            Console.WriteLine($@"Client {c}: {r.Token}");
+            return Task.CompletedTask;
+        });
+
+        var message = await File.ReadAllTextAsync("Requests/SubscribeRequest.json");
+        requestManager.HandleRequest(Guid.NewGuid(), message);
+
+        message = await File.ReadAllTextAsync("Requests/HandshakeRequest.json");
+        requestManager.HandleRequest(Guid.NewGuid(), message);
+
+        //await File.WriteAllTextAsync("Requests/HandshakeRequest.json", json, Encoding.UTF8);
+
+        //var req = JsonSerializer.Deserialize<Message>(json, options);
+
         var server = new WebSocketServer("http://+:11011/");
         await server.Start();
     }
